9ffc13acf56f0f97f2b0ea1335fb9ef4f6a3dfa2
Brian Watkins <brian.watkins@gmail.com>
2024-04-28 08:38:41 -0400

    Explain wait for nginx

--- kind/start-cluster.sh
+++ kind/start-cluster.sh
@@ -10,6 +10,8 @@ kind load docker-image xeyes:fun xserver:fun
 # Add an nginx ingress controller
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
+echo "Waiting for NGINX Ingress to be available ..."
+
 kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
@@ -20,6 +22,7 @@ kubectl apply -f ./pod.yml
 kubectl apply -f ./service.yml
 kubectl apply -f ./ingress.yml
 
+echo
 echo "All set!"
 echo "Go to http://localhost:8888/vnc/vnc.html in your browser!"
 echo "Note that you may need to configure the novnc websocket to use the path /vnc/websockify if you haven't already."
